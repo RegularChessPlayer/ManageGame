9767fd7a4dad7075081e9efcd31a7a3a889eb090
thiago rodrigues <thiagoengh@outlook.com>
2021-01-04 14:40:20 -0300

    add ManageGameApiTest

---
+++ ManageGameApiTest/Controllers/FriendControllerTest.cs
@@ -0,0 +1,80 @@
+﻿using ManageGameApi.Controllers;
+using ManageGameApi.Domain.DTO;
+using ManageGameApi.Domain.Input;
+using ManageGameApi.Domain.Output;
+using ManageGameApi.Services.Interfaces;
+using ManageGameApiTest.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ManageGameApiTest.Controllers
+{
+    public class FriendControllerTest
+    {
+        FriendController _controller;
+        IFriendService _service;
+
+        public FriendControllerTest()
+        {
+            _service = new FriendServiceFake();
+            _controller = new FriendController(_service);
+        }
+
+
+        [Fact]
+        public async Task Get_WhenCalled_ListFriendAsync()
+        {
+            // Act
+            var response = await _controller.GetFriendsAsync();
+            var okResult = response.Result as OkObjectResult;
+            // Assert
+            var items = Assert.IsType<List<FriendDTO>>(okResult.Value);
+            Assert.Equal(5, items.Count);
+        }
+
+        [Fact]
+        public async Task Post_WhenCalled_SaveFriendAsync()
+        {
+            //Asert
+            var friendInput = new FriendInput() { Name = "Jhon Al" };
+            // Act
+            var response = await _controller.PostCreateFriendAsync(friendInput);
+            var okResult = response as OkObjectResult;
+            // Assert
+            var friendDTO = Assert.IsType<FriendDTO>(okResult.Value);
+            Assert.Equal(friendInput.Name, friendDTO.Name);
+        }
+
+        [Fact]
+        public async Task Put_WhenCalled_UpdateFriendAsync()
+        {
+            //Asert
+            long idMock = 103l;
+            var friendInput = new FriendInput() { Name = "New Name" };
+            // Act
+            var response = await _controller.PutAsync(idMock, friendInput);
+            var okResult = response as OkObjectResult;
+            // Assert
+            var friendDTO = Assert.IsType<FriendDTO>(okResult.Value);
+            Assert.Equal(friendInput.Name, friendDTO.Name);
+        }
+
+        [Fact]
+        public async Task Delete_WhenCalled_DeleteFriendAsync()
+        {
+            //Asert
+            long idMock = 103l;
+            // Act
+            var response = await _controller.DeleteAsync(idMock);
+            var okResult = response as OkObjectResult;
+            // Assert
+            var friendDTO = Assert.IsType<FriendDTO>(okResult.Value);
+            Assert.Equal(idMock, friendDTO.Id);
+        }
+
+
+
+    }
+}
---
+++ ManageGameApiTest/Services/FriendServiceFake.cs
@@ -0,0 +1,75 @@
+﻿using AutoMapper;
+using ManageGameApi.Domain.DTO;
+using ManageGameApi.Domain.Entities;
+using ManageGameApi.Domain.Input;
+using ManageGameApi.Domain.Output;
+using ManageGameApi.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManageGameApiTest.Services
+{
+    class FriendServiceFake : IFriendService
+    {
+        private readonly List<Friend> _friendRepository;
+
+        public FriendServiceFake()
+        {
+            _friendRepository = new List<Friend>() {
+                new Friend{ Id = 101, Name = "João Batista" },
+                new Friend{ Id = 102, Name = "Maria Rita" },
+                new Friend{ Id = 103, Name = "Marcos Oliveira" },
+                new Friend{ Id = 104, Name = "Artur Rocha" },
+                new Friend{ Id = 105, Name = "Viviane Almeida" },
+            };
+        }
+
+        public Task<IEnumerable<FriendDTO>> ListFriendAsync()
+        {
+            IEnumerable<FriendDTO> friendsDTO = _friendRepository.Select(x => new FriendDTO { 
+                Id = x.Id,
+                Name = x.Name
+            }).ToList();
+            return Task.FromResult(friendsDTO);
+        }
+
+        public Task<FriendResponse>SaveFriendAsync(FriendInput friendInput)
+        {
+            var friend = new Friend() { Id = GeraId(), Name = friendInput.Name };
+            _friendRepository.Add(friend);
+            var friendResponse = new FriendResponse(friend);
+            return Task.FromResult(friendResponse);
+        }
+
+        public Task<FriendResponse> UpdateFriendAsync(long id, FriendInput friendInput)
+        {
+            var friend = _friendRepository.Find(x => x.Id == id);
+            friend.Name = friendInput.Name;
+            var friendResponse = new FriendResponse(friend);
+            return Task.FromResult(friendResponse);
+        }
+
+        public Task<FriendResponse> DeleteFriendAsync(long id)
+        {
+            var deleteFriend = _friendRepository.Find(x => x.Id == id);
+            _friendRepository.Remove(deleteFriend);
+            var friendResponse = new FriendResponse(deleteFriend);
+            return Task.FromResult(friendResponse);
+        }
+
+        public FriendDTO MapFriendToFriendDTO(Friend friend)
+        {
+            return new FriendDTO() { Id = friend.Id, Name = friend.Name };
+        }
+
+        static int GeraId()
+        {
+            Random random = new Random();
+            return random.Next(1, 100);
+        }
+
+    }
+}
